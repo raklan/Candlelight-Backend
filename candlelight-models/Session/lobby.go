401b71ccf8a96a8b382778c94a38c01b5dc51af0
Ryan Lake <rcl.catcher@gmail.com>
2024-10-11 11:32:53 -0600

    Support for ending games + improved logging for rejoin endpoint

--- candlelight-models/Session/lobby.go
+++ candlelight-models/Session/lobby.go
@@ -2,6 +2,12 @@ package Session
 
 import "candlelight-models/Player"
 
+const (
+	LobbyStatus_AwaitingStart = "Awaiting Start"
+	LobbyStatus_InProgress    = "In Progress"
+	LobbyStatus_Ended         = "Game Ended"
+)
+
 //A lobby is a collection of players waiting for a game to start. This is created by the /createRoom endpoint
 //which returns the room code. From there, you can pass the room code to /joinRoom which will put you in the room
 //and return the state of the lobby
@@ -13,6 +19,8 @@ type Lobby struct {
 	//The GameState created from this lobby. This field is empty until the game is started,
 	//at which point the API server will fill it in.
 	GameStateId string `json:"gameStateId"`
+	//The status of the game, really only used for the backend to determine whether a game has started/ended. Will be one of the above constants
+	Status string `json:"status"`
 	//Name of the Game being played
 	GameName string `json:"gameName"`
 	//Current number of players in the lobby
